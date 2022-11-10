35492a2fa905b6ef91c6738111775756086aebd7
Keks <keks@htmlacademy.ru>
2022-11-10 15:27:08 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import './util.js';
+import './popup.js';
+import './form.js';
+import {getPictures} from './data.js';
+
+getPictures();
---
+++ 6/js/util.js
@@ -0,0 +1,19 @@
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < 0 || min === max) {
+    return NaN;
+  }
+
+  if (min > max) {
+    return NaN;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const checkLineLenght = (line, maxLenght) => String.length < maxLenght;
+
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+// const isEnterKey = (evt) => evt.key === 'Enter';
+
+export {getRandomArrayElement, getRandomIntInclusive, checkLineLenght, isEscapeKey};
